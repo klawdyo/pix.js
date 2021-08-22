81a608a87c4b237e479cbf2f0f53e93237e5a5f4
Cláudio Medeiros <klawdyo@gmail.com>
2021-08-22 12:08:38 -0300

    feat: Arquivo index agora se refere ao dist

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
-const { pix, qrcode } = require('./src/pix');
-
-const payload = {
-  key: 'klawdyo@gmail.com',
-  name: 'Jose Claudio Medeiros de Lima',
-  city: 'AssuRN',
-  amount: 1,
-  zipcode: 59650000,
-  txId: 'SINASEFE-18/2021',
-};
-
-
-// Devolve a linha digitável do pix
-const code = pix(payload)
-console.log(code)
-
-
-// Devolve a imagem do qrcode em base64
-// qrcode(payload).then(url => console.log(url))
-
+const { pix, qrcode } = require('./dist/pix');
+module.exports = { pix, qrcode }
