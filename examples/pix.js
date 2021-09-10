27e56d3a8e2667a9f3346a5b6c952d613e17a296
Cláudio Medeiros <klawdyo@gmail.com>
2021-09-10 01:51:02 -0300

    docs: Atualização dos examplos

--- examples/pix.js
+++ examples/pix.js
@@ -4,28 +4,30 @@
 // Exemplo usando o código puro
 const { pix } = require('../src/pix');
 
-// const payload = {
-//   key: 'klawdyo@gmail.com',
-//   name: 'Jose Claudio Medeiros de Lima',
-//   city: 'Assú/RN',
-//   amount: 1,
-//   zipcode: 59650000,
-//   txId: '1X2Azzzzzzzz3',
-//   isUnique: false,
-//   description: '2 bananas e um milk-shake',
-// };
+const payload = {
+  key: 'klawdyo@gmail.com',
+  name: 'Jose Claudio Medeiros de Lima',
+  city: 'Assú/RN',
+  amount: 12.69,
+  zipcode: 59650000,
+  txId: '1X2Azzzzzzzz3',
+  isUnique: false,
+  description: 'Manutenção Mensal',
+};
+
+const keys = {
+  cpf: '170.803.140-54',
+  cnpj: '38.262.543/0001-50',
+  random: '3066362f-020c-4b46-9c1b-4ee3cf8a1bcc',
+  email: 'klawdyo@gmail.com',
+  phone: '+55 (84) 9 9696-4567',
+};
+
+const key = keys.phone;
+
+console.log({ ...payload, key });
 
 // // Devolve a linha digitável do pix
-// const code = pix(payload);
-const code = pix({
-  key: '3066362f-020c-4b46-9c1b-4ee3cf8a1bcc',
-  amount: 349.99,
-  txId: '12345',
-  name: 'Jose Claudio Medeiros de Lima',
-  city: 'Assu/RN',
-  zipcode: 59650000,
-  description: 'Parcela 1/24 - Sistema + Site',
-  isUnique: true,
-});
+const code = pix({ ...payload, key });
 
 console.log(code);
